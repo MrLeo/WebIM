8daeba897f9b482e9eae107177d0ae0463820af0
Leo <leo@xuebin.me>
2017-08-29 13:49:53 +0800

    no message

--- src/store/mutation-types.js
+++ src/store/mutation-types.js
@@ -1,7 +1,4 @@
-//region base module
-export const BASE_INIT_IM = 'base/initIM'
-export const BASE_SET_VERSION = 'base/setVersion'
-export const BASE_SET_USERINFO = 'base/setUserInfo'
-export const BASE_SET_FRIENDS='base/setFriends'
-//endregion
+export const BASE = {
+    initIM: 'base/initIM',
 
+}
